40251298b041473dd6a56bec8ea231f51fb177d5
psi <37362758+psi@users.noreply.github.com>
2018-12-07 15:30:00 +0900

    최초 commit

---
+++ app/src/main/java/com/kmu/memo_to_do/MemoListActivity.java
@@ -0,0 +1,13 @@
+package com.kmu.memo_to_do;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MemoListActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_list);
+    }
+}
